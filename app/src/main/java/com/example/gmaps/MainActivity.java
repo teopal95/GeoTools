31d683225a7569725248ed65e2297499fd6ab4d9
teopal95 <teopal95@gmail.com>
2021-12-01 21:13:08 +0200

    Create kml

--- app/src/main/java/com/example/gmaps/MainActivity.java
+++ app/src/main/java/com/example/gmaps/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -33,9 +34,14 @@ import org.xmlpull.v1.XmlSerializer;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -84,6 +90,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        
 
 
         if (requestCode == PICK_FILE) {
@@ -222,7 +230,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         startActivity(intent);
 
     }
-
+   
     private String readTextFile(Uri uri) {
         BufferedReader reader = null;
         StringBuilder builder = new StringBuilder();
@@ -238,6 +246,93 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             e.printStackTrace();
         }
         return builder.toString();
+    }
+
+    public void createKMLFile() {
+
+        String kmlName = editText.getText().toString();
+
+        String kmlInput = polygonString.replace("[", "").replace("lat/lng:", "").replace("(", "")
+                .replace(")", "").replace("]", "").replace(", "," ").replaceFirst("^ *", "").replaceAll(" +", " ");
+
+
+       kmlInput = kmlInput.replaceAll("(-?\\d+[.]\\d+),(-?\\d+[.]\\d+)", "$2,$1");
+
+
+
+
+        String kmlstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n" +
+                "<Document>";
+
+        String kmlelement = "<Placemark>"+"<ExtendedData>"+"</ExtendedData>"+"<Polygon>"+"<outerBoundaryIs>"+"<LinearRing>"+"<coordinates>"+kmlInput+"</coordinates>" +
+    "</LinearRing>"+"</outerBoundaryIs>"+"</Polygon>"+"</Placemark>";
+
+
+        String kmlend = "</Document>" +"</kml>";
+
+        ArrayList<String> content = new ArrayList<String>();
+        content.add(0, kmlstart);
+        content.add(1, kmlelement);
+        content.add(2, kmlend);
+
+        String kmltest = content.get(0) + content.get(1) + content.get(2);
+
+
+        File testexists = new File("/sdcard/download/KML" + "/" + kmlName + ".kml");
+        Writer fwriter;
+
+        if (!testexists.exists()) {
+            try {
+
+                fwriter = new FileWriter("/sdcard/download/KML" + "/" + kmlName + ".kml");
+                fwriter.write(kmltest);
+                fwriter.flush();
+                fwriter.close();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        } else {
+
+            //schleifenvariable
+            String filecontent = "";
+
+            ArrayList<String> newoutput = new ArrayList<String>();
+            ;
+
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(testexists));
+                while ((filecontent = in.readLine()) != null)
+
+                    newoutput.add(filecontent);
+
+            } catch (FileNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+            newoutput.add(2, kmlelement);
+
+            String rewrite = "";
+            for (String s : newoutput) {
+                rewrite += s;
+            }
+
+            try {
+                fwriter = new FileWriter("/sdcard/download/KML" + "/" + kmlName + ".kml");
+                fwriter.write(rewrite);
+                fwriter.flush();
+                fwriter.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
@@ -261,6 +356,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 
             Toast.makeText(this, "Land Saved", Toast.LENGTH_SHORT).show();
+
+            createKMLFile();
+
 
 
         } catch (Exception e) {
